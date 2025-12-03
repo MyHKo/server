c8a0a0419c7d14f44f2b457b1d1122b034f5fcc0
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-12-03 10:04:22 +0100

    Add race condition failure

--- src/main/threads.java
+++ src/main/threads.java
@@ -1,18 +1,33 @@
 public class threads{
+	static int a = 10;
 	public static void main(String[] args) {
 		
-		int a = 10;
 		
 		Runnable t1 = () -> {
-			a++;
-			System.out.println("a is " + a + " and it was updated by " + Thread.currentThread().getName());
+			int temp = a;
+			try {Thread.sleep(10);} catch (Exception e){} 
+			a = temp + 1;
+			System.out.println(a);
 		};
+		
 		
 		Thread worker = new Thread(t1);
 		Thread worker2 = new Thread(t1);
+		Thread worker3 = new Thread(t1);
+		Thread worker4 = new Thread(t1);
 		
 		worker.start();
 		worker2.start();
+		worker3.start();
+		worker4.start();
+		
+		while(true) {
+			if(!(worker.isAlive() || worker2.isAlive() || worker3.isAlive() || worker4.isAlive())) {
+				System.out.println(a);
+				break;
+			}
+		}
+		
 		
 
 	}
