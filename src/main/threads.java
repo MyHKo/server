509ed0c1ccfb614cf044eca14504dc92c5795530
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-12-03 09:43:03 +0100

    Modify the threads

--- src/main/threads.java
+++ src/main/threads.java
@@ -1,24 +1,19 @@
 public class threads{
 	public static void main(String[] args) {
 		
+		int a = 10;
+		
 		Runnable t1 = () -> {
-			System.out.println("Hello from Thread " + Thread.currentThread().getName());
-		};
-		Runnable t2 = () -> {
-			System.out.println("Hello from Thread " + Thread.currentThread().getName());
-		};
-		Runnable t3 = () -> {
-			System.out.println("Hello from Thread " + Thread.currentThread().getName());
+			a++;
+			System.out.println("a is " + a + " and it was updated by " + Thread.currentThread().getName());
 		};
 		
 		Thread worker = new Thread(t1);
-		Thread worker2 = new Thread(t2);
-		Thread worker3 = new Thread(t3);
+		Thread worker2 = new Thread(t1);
 		
 		worker.start();
 		worker2.start();
-		worker3.start();
 		
-		System.out.println("Hello from the main Thread");
+
 	}
 }
