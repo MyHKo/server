f13941a2027a88a764752fe7c65ce02e474d504b
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-12-12 22:52:20 +0100

    feat: add the connection loop

--- src/main/Hello.java
+++ src/main/Hello.java
@@ -1,6 +1,9 @@
 import java.nio.*;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 import java.net.InetSocketAddress;
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -12,6 +15,22 @@ public class Hello {
 	server.socket().bind(new InetSocketAddress(8080));
 	server.configureBlocking(false);
 	Selector selector = Selector.open();
-	System.out.println("Listening on a port 8080");	
+	
+	server.register(selector, SelectionKey.OP_ACCEPT);
+	
+	System.out.println("Listening on port 8080");
+	
+	while(true) {
+		selector.select();
+		
+		Set<SelectionKey> keys = selector.selectedKeys();
+		Iterator<SelectionKey> iterator = keys.iterator();
+		
+		while(iterator.hasNext()) {
+			SelectionKey key = iterator.next();
+			System.out.println("Request has been received");
+			iterator.remove();
+		}
+	}
 	}
 }
